da3d592a289a131cb71b6c1489b4b87e88c25fbc
Hector <sabatie@live.cl>
2024-08-24 17:42:29 -0400

    efecto javescrip

--- assets/contenido/javascrip.js
+++ assets/contenido/javascrip.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var iframe = document.getElementById("myIframe");
-    iframe.onload = function() {
-        
-        // Ajusta la altura del iframe al contenido
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-    }
+const img = document.getElementById('imagenCambia');
+
+img.addEventListener('mouseover', function() {
+    img.style.transition = 'transform 1s ease';
+    img.style.transform = 'scale(1.05)'
+    img.src = '../imagenes/solo leveling 3.webp'; // Cambia a la imagen cuando el ratón pasa por encima
+});
+
+img.addEventListener('mouseout', function() {
+    img.style.transform = 'scale(1)'; // Vuelve al tamaño original
+    img.style.transition = 'transform 1s ease'; // Asegura que la transición se aplique
+    img.src = '../imagenes/inicios.png'; // Vuelve a la imagen original cuando el ratón sale
+    
 });
